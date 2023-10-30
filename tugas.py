2154d480e5fa5ebfd53a741abe2fadaa1d632824
setiawansendi <146158584+setiawansendi@users.noreply.github.com>
2023-10-30 21:04:57 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,17 @@
+print("no 2")
+for i in range(1,2):
+    for i in range(i):
+        print("*", end=" ")
+        print()
+for i in range(1,3):
+    for i in range(i):
+        print("**", end=" ")
+        print()
+for i in range(1,4):
+    for i in range(i):
+        print("***", end=" ")
+        print()
+for i in range(1,5):
+    for i in range(i):
+        print("****", end=" ")
+        print()
